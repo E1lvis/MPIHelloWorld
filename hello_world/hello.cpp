142284714c5a1229188538ef2b0f0858428c2e47
Elvis F Velasquez Euceda <evelasq2@gal-i1.uncc.edu>
2021-11-10 16:28:06 -0500

    ran bench

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -11,7 +11,7 @@ int totalProcesses;
 MPI_Comm_rank(MPI_COMM_WORLD, &processNumber);
 MPI_Comm_size(MPI_COMM_WORLD, &totalProcesses);
 
-std::cout<<"I am process "<< processNumber << " out of " << totalProcesses << " ";
+std::cout<<"I am process "<< processNumber << " out of " << totalProcesses << " \n";
 
 MPI_Finalize();
 }
