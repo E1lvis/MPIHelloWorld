59b191e4159b792fa4bd8f2ebd421475c241204c
Elvis F Velasquez Euceda <evelasq2@gal-i1.uncc.edu>
2021-11-11 18:36:22 -0500

    added machine name

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -7,11 +7,14 @@ MPI_Init(&argc, &argv);
 
 int processNumber;
 int totalProcesses;
+int processorNameSize;
+char processorName[MPI_MAX_PROCESSOR_NAME];
 
 MPI_Comm_rank(MPI_COMM_WORLD, &processNumber);
 MPI_Comm_size(MPI_COMM_WORLD, &totalProcesses);
+MPI_Get_processor_name(processorName, &processorNameSize);
 
-std::cout<<"I am process "<< processNumber << " out of " << totalProcesses << " \n";
+std::cout<<"I am process "<< processNumber << " out of " << totalProcesses << ". I am running on machine" << processorName << ". \n";
 
 MPI_Finalize();
 }
